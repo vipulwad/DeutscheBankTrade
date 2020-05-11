07b25558e710077ea9915e3a6642f0edec62ccb4
Vipul <vipulw@yahoo.com>
2020-05-11 21:07:42 +0530

    correct package

---
+++ src/test/java/com/vipul/DeutscheBankTrade/SuiteTest.java
@@ -0,0 +1,25 @@
+package com.vipul.DeutscheBankTrade;
+
+import com.vipul.DeutscheBankTrade.CSVToTradeInputDataAdaptorTest;
+import com.vipul.DeutscheBankTrade.Store.DataStoreRulesTest;
+import com.vipul.DeutscheBankTrade.Store.VersionRuleTest;
+import com.vipul.DeutscheBankTrade.TradeInputData.MaturityDateRule;
+import com.vipul.DeutscheBankTrade.TradeInputData.MaturityDateRuleTest;
+import com.vipul.DeutscheBankTrade.TradeInputData.TradeInputDataTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                CSVToTradeInputDataAdaptorTest.class,
+                MaturityDateRuleTest.class,
+                DataStoreRulesTest.class,
+                VersionRuleTest.class,
+                TradeInputDataTest.class
+
+        }
+)
+public class SuiteTest {
+
+}
